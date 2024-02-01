1c87c156ca38eb8eea8c4c378ceffb77a50c750d
Abhishek Joshi <90437046+AbhishekJ24@users.noreply.github.com>
2024-02-01 11:51:44 +0530

    Added music directory, music cards under library, playbar and did some stylesheet adjustments

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 addEventListener("contextmenu", event => event.preventDefault())
+
+async function main(){
+    let response=await fetch("http://127.0.0.1:3000/music/")
+    let songs_directory=await response.text()
+    let div=document.createElement('div')
+    div.innerHTML=songs_directory
+    let as = div.getElementsByTagName("a")
+    let songs=[]
+    for (let i = 0; i < as.length; i++) {
+        if(as[i].href.endsWith('.mp3'))
+        songs.push(as[i].href)
+    }
+    console.log(songs)
+}
+
+main()
